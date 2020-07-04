a16b2f19e0278ed9b707dae5721a0ccd8b2e0b09
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-07-04 16:37:54 -0500

    scroll

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+$(window).scroll(function () {
+  $(".top").css("opacity", 1 - $(window).scrollTop() / 50);
+});
