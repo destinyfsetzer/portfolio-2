6800d6a8dfcac74bc3260226890d561557b9a441
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-07-11 17:27:44 -0500

    added footer icons

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// function myFunction() {
-//   var x = document.getElementById("myLinks");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
 $(window).scroll(function () {
   $(".top").css("opacity", 1 - $(window).scrollTop() / 50);
 });
